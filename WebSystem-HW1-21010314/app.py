a7b5406d5e496a3c2b2a93e9e86bbc220f5f05b4
전해찬 <haechan8864@naver.com>
2024-10-04 12:13:36 +0900

    여기까지 프론트 작성 완료 프론트에서 다시 해야할 것 1. 7개 정도로 페이지를 잘라서 나타낼 수 있도록 하기  -> 이거는 백에서 하는건가? ㅁㄹ겠네 ㅇㅇ

--- WebSystem-HW1-21010314/app.py
+++ WebSystem-HW1-21010314/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask import render_template
 
@@ -23,144 +23,22 @@ class Todo(db.Model):
         return f'<Todo {self.title}>'
 
 
-@app.route('/todo', methods=['POST'])
-def add_todo():
-    data = request.json
-    if not data or 'title' not in data:
-        return jsonify({'error': 'Title is required'}), 400
-    new_todo = Todo(
-            title=data['title'],
-            description=data.get('description','')
-            )
-    db.session.add(new_todo)
-    db.session.commit()
-    return jsonify({
-            'id': new_todo.id,
-            'title': new_todo.title,
-            'description': new_todo.description,
-            'completed': new_todo.completed,
-            'created_at': new_todo.created_at}), 201
-
-# 다 가져오는 거
-@app.route('/todos', methods=['GET'])
-def get_todo():
-    todos = Todo.query.all()
-    return jsonify([
-        {
+@app.route('/')
+def index():
+    tmp = Todo.query.all()
+    todos = []
+    for todo in tmp:
+        todos.append({
             'id': todo.id,
             'title': todo.title,
             'description': todo.description,
             'completed': todo.completed,
             'created_at': todo.created_at
-        } for todo in todos
-        ])
+        })
+    return render_template('index.html', todos=todos)
 
-# 인덱스로 특정 하나 조회
-@app.route('/todo/<int:id>', methods=['GET'])
-def get_todo01(id):
-    todo = Todo.query.get_or_404(id)
-    return jsonify({
-        'id' : todo.id,
-        'title' : todo.title,
-        'description' : todo.description,
-        'completed' : todo.completed,
-        'created_at' : todo.created_at
-    })
 
-@app.route('/todo/<int:id>', methods=['PUT'])
-def update_todo(id):
-    todo = Todo.query.get_or_404(id)
-    data = request.json
-    todo.title = data.get('title', todo.title)
-    todo.description = data.get('description', todo.description)
-    todo.completed = data.get('completed', todo.completed)
-
-    db.session.commit()
-    return jsonify({
-        'id': todo.id,
-        'title': todo.title,
-        'description': todo.description,
-        'completed': todo.completed,
-        'created_at': todo.created_at
-    })
-
-@app.route('/todo/<int:id>', methods=['DELETE'])
-def delete_todo(id):
-    todo = Todo.query.get_or_404(id)
-    db.session.delete(todo)
-    db.session.commit()
-    return jsonify({'message': 'Todo deleted successfully'}), 200
-
-@app.route('/todos/completed', methods=['GET'])
-def get_completed_todos():
-    completed_todos = Todo.query.filter_by(completed=True).all()
-    # completed_todos = Todo.query.filter_by(completed=False).all()
-    return jsonify([
-        {
-        'id': todo.id,
-        'title': todo.title,
-        'description': todo.description,
-        'created_at': todo.created_at
-        } for todo in completed_todos
-    ])
-
-@app.route('/todos/sorted', methods=['GET'])
-def get_sorted_todos():
-    # asc : 오름 / desc : 내림
-    sorted_todos = Todo.query.order_by(Todo.id.asc()).all()
-    return (jsonify
-        ([
-            {
-                'id': todo.id,
-                'title': todo.title,
-                'description': todo.description,
-                'completed': todo.completed,
-                'created_at': todo.created_at
-            }
-            for todo in sorted_todos
-        ]))
-
-@app.route('/todos/paginated', methods=['GET'])
-def get_paginated_todos():
-    page = request.args.get('page', 1, type=int)
-    per_page = request.args.get('per_page', 2, type=int)
-    paginated_todos = Todo.query.paginate(page=page, per_page=per_page, error_out=False)
-    return (jsonify
-        ({
-        'todos':
-            [
-                {
-                    'id': todo.id,
-                    'title': todo.title,
-                    'description': todo.description,
-                    'completed': todo.completed,
-                    'created_at': todo.created_at
-                }
-                for todo in paginated_todos.items
-            ],
-                'total': paginated_todos.total,
-                'pages': paginated_todos.pages,
-                'current_page': page
-        }))
-
-@app.route('/todos/search', methods=['GET'])
-def search_todos():
-    # ?keyword=검색어 이렇게 받아 오는 것
-    keyword = request.args.get('keyword', '')
-    todos = Todo.query.filter(
-        Todo.title.like(f'%{keyword}'),
-        Todo.completed == False
-    ).all()
-    return jsonify([
-        {
-            'id': todo.id,
-            'title': todo.title,
-            'description': todo.description,
-            'completed': todo.completed,
-            'created_at': todo.created_at
-        } for todo in todos
-    ])
-
+# 
 with app.app_context():
     db.create_all()
 
